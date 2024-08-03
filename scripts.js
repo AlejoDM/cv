0bfc8eecfe748a2fd9f776ba4f9942f090b928ba
Alejo <alejodm12345@gmail.com>
2024-08-03 17:35:39 -0300

    Habilitar y deshabilitar secciones.

--- scripts.js
+++ scripts.js
@@ -5,10 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
 function showSection(sectionId) {
     const sections = document.querySelectorAll("main section");
     sections.forEach(section => {
-        if (section.id === sectionId) {
-            section.classList.add("active");
-        } else {
-            section.classList.remove("active");
+        if (section.id !== 'inicio') { // Mantener siempre visible la sección de inicio
+            if (section.id === sectionId) {
+                section.classList.add("active");
+                section.classList.remove("hidden");
+            } else {
+                section.classList.add("hidden");
+                section.classList.remove("active");
+            }
         }
     });
 }
